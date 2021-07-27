804c48fe5c6f237c796f76257a45e70dfef9b7fe
samr <79730498+samarthind@users.noreply.github.com>
2021-07-27 16:12:03 -0700

    It's Big Brain Time
    /Coding/Python/math.py is the real Big Brain Moment.
    ---
    Now, you can do math using the all-new Big Brain Time program, written using Python! No need for a calculator, when you have Python!

---
+++ Coding/Python/math.py
@@ -0,0 +1,8 @@
+print("It's Big Brain Time")
+x = input("Type a number: ")
+y = input("Type another number: ")
+
+sum = int(x) + int(y)
+
+print("The sum is: ", sum)
+input("Press the Enter key to exit...")
