e9f640667d566b018bb026c867fd34f4c6e991ad
samr <79730498+samarthind@users.noreply.github.com>
2021-08-18 08:18:50 -0700

    C++

---
+++ Coding/C++/io.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!";
+    cout << "This is a test program.";
+    string x = "";
+    cout << "Input: ";
+    cin >> x;
+    cout << "Your input was: " + x;
+}
