24561ce7cec91345b0b9acf34aa6b836de28d122
samarthind <samarveerthind13@outlook.com>
2021-08-27 13:55:34 -0700

    More Lua

---
+++ Coding/Lua/charcount.lua
@@ -0,0 +1,5 @@
+print("Use this tool to count the amount of characters in a word or sentence.")
+print("Type your sequence: ")
+userInput = io.read()
+charcount = string.len(userInput)
+print("The amount of characters in ", userInput, " is: ", charcount)
