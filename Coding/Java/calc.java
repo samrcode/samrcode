b5677c50c4d71d06053ec4363381b8a2767dcbf5
samr <79730498+samarthind@users.noreply.github.com>
2021-09-01 23:00:53 +0000

    Java

---
+++ Coding/Java/calc.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+class calc {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int z;
+		System.out.println("Enter an operator: ");
+		String op = input.nextLine();
+		System.out.println("Enter a number: ");
+		String stringX = input.nextLine();
+		System.out.println("Enter another number: ");
+		String stringY = input.nextLine();
+		int x = Integer.parseInt(stringX);
+		int y = Integer.parseInt(stringY);
+		if (op == "+") {
+			try {
+				z = x + y;
+				System.out.println("The sum is: " + z);
+			} catch (Exception e) {
+				System.out.println("Something went wrong. Try again later.");
+			}
+		} else if (op == "-") {
+			try {
+				z = x - y;
+				System.out.println("The sum is: " + z);
+			} catch (Exception e) {
+				System.out.println("Something went wrong. Try again later.");
+			}
+		} else if (op == "*") {
+			try {
+				z = x * y;
+				System.out.println("The sum is: " + z);
+			} catch (Exception e) {
+				System.out.println("Something went wrong. Try again later.");
+			}
+		} else if (op == "/") {
+			try {
+				z = x / y;
+				System.out.println("The sum is: " + z);
+			} catch (Exception e) {
+				System.out.println("Something went wrong. Try again later.");
+			}
+		} else {
+			System.out.println("Something happened. Try again later.");
+		}
+	}
+}
