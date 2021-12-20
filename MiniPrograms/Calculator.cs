0c45d92dafc3ea63f5ac9988f6bcd75644a20c3e
samr <samr.stuff@pm.me>
2021-12-19 17:43:40 -0800

    Update Calculator.cs

--- MiniPrograms/Calculator.cs
+++ MiniPrograms/Calculator.cs
@@ -6,67 +6,57 @@ using System;
 
 class Calculator
 {
-	public static void Main(string[] args)
+	public static void Calculate(int X, int Y)
 	{
-		Console.WriteLine("Calculator");
-		Console.WriteLine("Type an operator: ");
-		Console.WriteLine("[1] Addition");
-		Console.WriteLine("[2] Subtraction");
-		Console.WriteLine("[3] Multiplication");
-		Console.WriteLine("[4] Division");
-		string op = Console.ReadLine();
-		Console.WriteLine("Type a number: ");
-		int x = Convert.ToInt32(Console.ReadLine());
-		Console.WriteLine("Type another number: ");
-		int y = Convert.ToInt32(Console.ReadLine());
-		if (op == "1")
+		// Calculates values and things
+		if (Op == "1")
 		{
 			// Addition
 			try
 			{
-				int z = x + y;
-				Console.WriteLine("The sum is: " + z);
+				int Z = X + Y;
+				Console.WriteLine("The sum is: " + Convert.ToString(Z));
 			}
-			catch (Exception e)
+			catch (Exception E)
 			{
 				Console.WriteLine("Something went wrong.");
 			}
 		}
-		else if (op == "2")
+		else if (Op == "2")
 		{
 			// Subtraction
 			try
 			{
-				int z = x - y;
-				Console.WriteLine("The sum is: " + z);
+				int Z = X - Y;
+				Console.WriteLine("The sum is: " + Convert.ToString(Z));
 			}
-			catch (Exception e)
+			catch (Exception E)
 			{
 				Console.WriteLine("Something went wrong.");
 			}
 		}
-		else if (op == "3")
+		else if (Op == "3")
 		{
 			// Multiplication
 			try
 			{
-				int z = x * y;
-				Console.WriteLine("The sum is: " + z);
+				int Z = X * Y;
+				Console.WriteLine("The sum is: " + Convert.ToString(Z));
 			}
-			catch (Exception e)
+			catch (Exception E)
 			{
 				Console.WriteLine("Something went wrong.");
 			}
 		}
-		else if (op == "4")
+		else if (Op == "4")
 		{
 			// Division
 			try
 			{
-				int z = x / y;
-				Console.WriteLine("The sum is: " + z);
+				int Z = X / Y;
+				Console.WriteLine("The sum is: " + Convert.ToString(Z));
 			}
-			catch (Exception e)
+			catch (Exception E)
 			{
 				Console.WriteLine("Something went wrong.");
 			}
@@ -76,5 +66,20 @@ class Calculator
 			// Error
 			Console.WriteLine("Something went wrong.");
 		}
+	}
+	public static void Main(string[] args)
+	{
+		Console.WriteLine("Calculator");
+		Console.WriteLine("Type an operator: ");
+		Console.WriteLine("[1] Addition");
+		Console.WriteLine("[2] Subtraction");
+		Console.WriteLine("[3] Multiplication");
+		Console.WriteLine("[4] Division");
+		string Op = Console.ReadLine();
+		Console.WriteLine("Type a number: ");
+		int X = Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine("Type another number: ");
+		int Y = Convert.ToInt32(Console.ReadLine());
+		Calculate(Op, X, Y)
 	}
 }
